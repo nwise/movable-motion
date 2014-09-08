0031338388a5ee13a5e4af22a58a7a9e86363ca6
Nathan Wise <nathanwise@gmail.com>
2014-09-08 09:57:09 -0400

    Adding more properties to User Screen

--- app/controllers/user_controller.rb
+++ app/controllers/user_controller.rb
@@ -12,6 +12,51 @@ class UserController < UIViewController
     super
     self.title = "#{self.user.first_name} #{self.user.last_name}"
 
+    self.edgesForExtendedLayout = UIRectEdgeNone
+
+    @info_container = UIView.alloc.initWithFrame [[0, 0], [self.view.frame.size.width, self.view.frame.size.height]]
+    @info_container.backgroundColor = UIColor.whiteColor
+    self.view.addSubview @info_container
+
+    @email_label = UILabel.alloc.initWithFrame [[10, 30], [0, 0]]
+    @email_label.text = "Email:"
+    @email_label.sizeToFit
+    self.view.addSubview @email_label
+
+    @email_value = UILabel.alloc.initWithFrame [[70, 30], [0, 0]]
+    @email_value.text = self.user.email
+    @email_value.sizeToFit
+    self.view.addSubview @email_value
+
+    @dob_label = UILabel.alloc.initWithFrame [[10, 60], [0, 0]]
+    @dob_label.text = "DOB:"
+    @dob_label.sizeToFit
+    self.view.addSubview @dob_label
+
+    @dob_value = UILabel.alloc.initWithFrame [[70, 60], [0, 0]]
+    @dob_value.text = self.user.date_of_birth
+    @dob_value.sizeToFit
+    self.view.addSubview @dob_value
+
+    @id_label = UILabel.alloc.initWithFrame [[10, 90], [0, 0]]
+    @id_label.text = "ID:"
+    @id_label.sizeToFit
+    self.view.addSubview @id_label
+
+    @id_value = UILabel.alloc.initWithFrame [[70, 90], [0, 0]]
+    @id_value.text = self.user.id.to_s
+    @id_value.sizeToFit
+    self.view.addSubview @id_value
+    
+    @height_label = UILabel.alloc.initWithFrame [[10, 120], [0, 0]]
+    @height_label.text = "Height:"
+    @height_label.sizeToFit
+    self.view.addSubview @height_label
+
+    @height_value = UILabel.alloc.initWithFrame [[70, 120], [0, 0]]
+    @height_value.text = self.user.height.to_s + " in."
+    @height_value.sizeToFit
+    self.view.addSubview @height_value
   end
 
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,5 +1,5 @@
 class User
-  PROPERTIES = [:id, :first_name, :last_name]
+  PROPERTIES = [:id, :first_name, :last_name, :email, :date_of_birth, :height]
   PROPERTIES.each do |prop|
     attr_accessor prop
   end
@@ -13,6 +13,16 @@ class User
   end
 
   def self.find(user_id, &block)
+    body = { client_id: '4a8418570d7c9f2586d8af9d25a774086694cfc06eb7de24d5f7534af80f06d1',
+             client_secret: '75efd068fedd84e2717933f7816db2e7b89248a8821a30da1048af281e3755f4',
+             redirect_uri: 'portal.movable.com',
+             grant_type: 'authorization_code',
+             code: '148f28cef62e530799059799cceddfdf7f475525d97ef738033c134558e39289'}
+
+    BW::HTTP.post("http://api.local:3000/oauth/token", payload: body) do |response|
+      puts response.body.to_s
+    end
+
     BW::HTTP.get("http://api.local:3000/v2/users/#{user_id}") do |response|
       result_data = BW::JSON.parse(response.body.to_str)
       puts result_data
